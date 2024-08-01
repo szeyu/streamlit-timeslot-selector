b5d3aee7910e10e54a4ab4d5044b85dc442e1c25
szeyu <szeyusim@gmail.com>
2024-08-01 13:52:37 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamlit-timeslot-selector",
+    version="0.0.1",
+    author="Sze Yu Sim",
+    author_email="szeyu.sim@embeddedllm.com",
+    description="Streamlit component that allows you to select timeslot",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'streamlit>=0.63',
+    ],
+)
